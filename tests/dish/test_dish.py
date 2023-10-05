2ccc475f7d6b217c8926f1748646899f0566de55
Ruan-Portella <ruanmorales29@gmail.com>
2023-10-05 15:10:54 -0300

    Feat: Requisito 02 Concluído

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,31 @@
-from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient
+import pytest
 
 
 # Req 2
 def test_dish():
-    pass
+    dish = Dish("Comida", 10.0)
+    assert dish.__repr__() == "Dish('Comida', R$10.00)"
+
+    ingredient = Ingredient("ovo")
+    dish.add_ingredient_dependency(ingredient, 2)
+    assert dish.name == "Comida"
+
+    dish_2 = Dish("Comida2", 10.0)
+
+    assert dish.__hash__() != dish_2.__hash__()
+    assert dish.__hash__() == dish.__hash__()
+
+    assert dish.__eq__(dish_2) is False
+    assert dish.__eq__(dish) is True
+
+    with pytest.raises(TypeError):
+        Dish("Comida", "10.0")
+
+    with pytest.raises(ValueError):
+        Dish("Comida", -10.0)
+
+    assert dish.get_restrictions() == ingredient.restrictions
+
+    assert dish.get_ingredients() == {ingredient}
